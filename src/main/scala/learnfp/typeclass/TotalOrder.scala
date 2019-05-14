a503bba35db1134ed5d17151db7e74a277168bb5
Chris Fisher <chris.i.fisher@gmail.com>
2019-05-14 19:36:13 +1200

    Add implementation for TotalOrder

--- src/main/scala/learnfp/typeclass/TotalOrder.scala
+++ src/main/scala/learnfp/typeclass/TotalOrder.scala
@@ -1,26 +1,24 @@
 package learnfp.typeclass
 
 trait TotalOrder[A] {
-  def less(lhs:A, rhs:A):Boolean
+  def less(lhs: A, rhs: A): Boolean
 }
 
 object TotalOrderInstances {
-  implicit val intInstance:TotalOrder[Int] = new TotalOrder[Int] {
-    override def less(lhs: Int, rhs: Int): Boolean = ???
-  }
+  implicit val intInstance: TotalOrder[Int] = (lhs: Int, rhs: Int) => lhs < rhs
 
-  implicit val stringInstance:TotalOrder[String] = new TotalOrder[String] {
-    override def less(lhs: String, rhs: String): Boolean = ???
-  }
+  implicit val stringInstance: TotalOrder[String] =
+    (lhs: String, rhs: String) => lhs.toInt < rhs.toInt
 
-  implicit def listInstance[T](implicit suborder:TotalOrder[T]):TotalOrder[List[T]] = new TotalOrder[List[T]] {
-    override def less(lhs: List[T], rhs: List[T]): Boolean = ???
-  }
+  implicit def listInstance[T](
+      implicit suborder: TotalOrder[T]
+  ): TotalOrder[List[T]] =
+    (lhs: List[T], rhs: List[T]) =>
+      lhs.zip(rhs).forall(t => suborder.less(t._1, t._2))
 }
 
 object Comparator {
-  @annotation.implicitNotFound("No instance of TotalOrder found")
-  def less[A](lhs:A, rhs:A)(implicit order:TotalOrder[A]) = {
+  def less[A](lhs: A, rhs: A)(implicit order: TotalOrder[A]): Boolean = {
     order.less(lhs, rhs)
   }
 }
--- src/main/scala/learnfp/typeclass/TypeClass.scala
+++ src/main/scala/learnfp/typeclass/TypeClass.scala
@@ -5,13 +5,9 @@ trait TypeClass[A] {
 }
 
 object TypeClassInstances {
-  implicit val intInstance:TypeClass[Int] = new TypeClass[Int] {
-    override def foo(x:Int): String = "int"
-  }
+  implicit val intInstance:TypeClass[Int] = (_: Int) => "int"
 
-  implicit val stringInstance:TypeClass[String] = new TypeClass[String] {
-    override def foo(x:String): String = "string"
-  }
+  implicit val stringInstance:TypeClass[String] = (_: String) => "string"
 }
 
 object TypeClassUser {
